ce61522f049eef35c7bb6b7f621cdf5698aa57b2
Richard Iyama <richardiyama@yahoo.com>
2020-09-25 13:27:17 +0100

    Added Implementation for Product and Home Page

--- src/Home.js
+++ src/Home.js
@@ -8,69 +8,63 @@ function Home() {
         <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
         alt="" />
 
-        <div className="home__row">
-
-        <Product 
-     id="12321341"
-     title="The Lean Startup: How Constant Innovation Creates"
-     price={11.96}
-     rating={5}
-     image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
-     />
-
-<Product 
-     id="12321341"
-     title="The Lean Startup: How Constant Innovation Creates"
-     price={11.96}
-     rating={5}
-     image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
-     />
-
-        </div>
-     
-        <div className="home__row">
-
-<Product 
-id="12321341"
-title="The Lean Startup: How Constant Innovation Creates"
-price={11.96}
-rating={5}
-image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
-/>
-
-<Product 
-id="12321341"
-title="The Lean Startup: How Constant Innovation Creates"
-price={11.96}
-rating={5}
-image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
-/>
-
-<Product 
-id="12321341"
-title="The Lean Startup: How Constant Innovation Creates"
-price={11.96}
-rating={5}
-image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
-/>
-
-
-</div>
-
 <div className="home__row">
+          <Product
+            id="8824324"
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            title="The Lean Startup: How Today's Entrepreneurs Use Continuous Innovation to Create Radically Successful Businesses"
+            vendor="Amazon"
+            price={19.99}
+            rating={5}
+          />
+          <Product
+            id="9325343"
+            image="https://images-na.ssl-images-amazon.com/images/I/41CRnvYqmqL._AC_SX184_.jpg"
+            title="Echo Dot (3rd Gen) - Smart speaker with Alexa"
+            vendor="Amazon"
+            price={49.99}
+            rating={4}
+          />
+    
 
-<Product 
-id="12321341"
-title="The Lean Startup: How Constant Innovation Creates"
-price={11.96}
-rating={5}
-image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_B01,204,203,200_.jpg"
-/>
+       
+          <Product
+            id="8934233"
+            image="https://images-na.ssl-images-amazon.com/images/I/31dYBKysp6L._AC_SX184_.jpg"
+            title="Ring Indoor Cam, Works with Alexa - White"
+            vendor="Ring"
+            price={59.99}
+            rating={4}
+          />
+          <Product
+            id="753068"
+            image="https://images-na.ssl-images-amazon.com/images/I/31l2mq9kTHL._AC_SX184_.jpg"
+            title="Echo Frames - Eyeglasses with Alexa - Black"
+            vendor="Amazon"
+            price={179.99}
+            rating={0}
+          />
+          <Product
+            id="6574836"
+            image="https://m.media-amazon.com/images/I/61moEqqnwzL._AC_UY218_.jpg"
+            title="Kindle Oasis – Now with adjustable warm light"
+            vendor="Amazon"
+            price={269.99}
+            rating={5}
+          />
+    
 
-
+        
+          <Product
+            id="123424"
+            image="https://m.media-amazon.com/images/I/81v90JtbImL._AC_UY218_.jpg"
+            title='Samsung CJ89 43" Curved UltraWide 3840 X 1200 Resolution 120Hz Monitor'
+            vendor="Samsung Store"
+            price={1096.59}
+            rating={5}
+          />
+        
 </div>
-
-
         </div>
     )
 }
